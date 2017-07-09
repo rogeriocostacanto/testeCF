6e329b2fce23b7e7abbd62ef571ca2dd5b672355
Rogerio Canto <rogeriocostacanto@gmail.com>
2017-07-09 13:39:39 -0300

    Listando Usuarios

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row">
-        {!! Table::withContents($users->items())->striped !!}
+        {!! Table::withContents($users->items())->striped() !!}
     </div>
+        {!! $users->links() !!}
 </div>
 @endsection
